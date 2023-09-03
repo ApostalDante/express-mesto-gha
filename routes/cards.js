4c2984b4ad54cc9a0164a10b4f62ec7b5ae2ac40
ApostalDante <apostal666666@gmail.com>
2023-09-03 08:42:37 +0300

    hotfix/react-mesto-gha-review-test8

--- routes/cards.js
+++ routes/cards.js
@@ -13,8 +13,8 @@ const {
   validateCard,
 } = require('../utils/validation');
 
-router.post('/', validateCreateCard, createCard);
 router.get('/', getCards);
+router.post('/', validateCreateCard, createCard);
 router.delete('/:cardId', validateCard, deleteCard);
 router.put('/:cardId/likes', validateCard, likeCard);
 router.delete('/:cardId/likes', validateCard, dislikeCard);
